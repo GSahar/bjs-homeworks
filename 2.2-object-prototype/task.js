7180cf5aff97800fe4fd390bea3a36b7830187bc
GSahar <andrey_144_sahar@mail.ru>
2020-08-17 22:05:29 +0500

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,44 @@
-//String.prototype.isPalindrome - для задачи №1
+//для задачи №1
+String.prototype.isPalindrome = function () {
+    let thisString = this;
+    let arrString = thisString.split('');
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    //Очищаем от пробелов
+    for (let i = 0; i < arrString.length; i++) {
+        if (arrString[i] === ' ') {
+            arrString.splice(i, 1);
+        }
+    }
+
+    let originString = arrString.join('').toLowerCase();
+    let reversString = arrString.reverse().join('').toLowerCase();
+
+    return reversString === originString;
 }
 
+// код для задачи №2 писать здесь
+function getAverageMark(marks) {
+
+    let sumArr = 0;
+    let result = 0;
+    let sizeArr = marks.length;
+
+    if (sizeArr !== 0) {
+        for (let i = 0; i < sizeArr; i++) {
+            sumArr = sumArr + marks[i];
+        }
+        result = sumArr / sizeArr;
+    }
+
+    return Math.round(result);
+}
+
+// код для задачи №3 писать здесь
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    //Один год в милисекундах
+    let oneYear = 31536000000;
+
+    let agePerson = (new Date() - new Date(birthday)) / oneYear;
+
+    return agePerson > 18 ? true : false;
 }
